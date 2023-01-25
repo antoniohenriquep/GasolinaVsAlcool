2a4c35547e29a2dc2665bd9df4c0f2cbd70ffb16
Antonio Pedreira <antoniohenriquep@outlook.com>
2023-01-25 14:17:03 -0300

    Inserir inputs

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Pressable, TextInput, Modal } from "react-native";
 
 export default function App()
 {
+    const [gasolina, setGasolina] = useState()
+    const [alcool, setAlcool] = useState()
+
+    
+
+
     return(
         <View style = {styles.container}>
             <View style = {styles.imgArea}>
@@ -10,6 +16,20 @@ export default function App()
                 </View>
                 <Text>Qual a melhor opção?</Text>
             </View>
+            <Text>Alcool</Text> <Text>(preço por litro)</Text>
+            <TextInput
+            keyboardType='decimal-pad' 
+            onChangeText={(text) => setAlcool(text)}
+            placeholder = "Digite o valor do álcool"
+             />
+
+            <Text>Gasolina</Text> <Text>(preço por litro)</Text>
+            <TextInput 
+            keyboardType='decimal-pad'
+            onChangeText={(text) => setGasolina(text)}
+            placeholder = "Digite o valor da gasolina"
+             />
+
         </View>
     )
 }
@@ -23,5 +43,9 @@ const styles = StyleSheet.create({
         justifyContent:'center',
         alignItems:'center',
         padding:30
+    },
+    title:{
+        fontSize:32,
+        fontWeight:'bold'
     }
 })
